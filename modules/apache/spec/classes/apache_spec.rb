75d06f7808e09a129c9d3d061e1e967568468cf9
Will Sheppard <will.sheppard.uk@gmail.com>
2019-12-09 09:30:31 +0000

    Remove code to simplify example

--- modules/apache/spec/classes/apache_spec.rb
+++ modules/apache/spec/classes/apache_spec.rb
@@ -17,41 +17,4 @@ describe 'apache', :type => 'class' do
     }
   end
 
-  context "On a RedHat OS with no package name specified" do
-    let :facts do
-      {
-        :osfamily => 'RedHat'
-      }
-    end
-
-    it {
-      should contain_package('httpd').with( { 'name' => 'httpd' } )
-      should contain_service('httpd').with( { 'name' => 'httpd' } )
-    }
-  end
-
-  context "On an unknown OS with no package name specified" do
-    let :facts do
-      {
-        :osfamily => 'Darwin'
-      }
-    end
-
-    it {
-      expect { should raise_error(Puppet::Error) }
-    }
-  end
-
-  context "With a package name specified" do
-    let :params do
-      {
-        :package_name => 'apache'
-      }
-    end
-
-    it {
-      should contain_package('httpd').with( { 'name' => 'apache' } )
-      should contain_service('httpd').with( { 'name' => 'apache' } )
-    }
-  end
 end
